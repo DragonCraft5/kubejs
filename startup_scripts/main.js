9f0e8cade797f4d3c51ec57cea8708be3a529bfe
Novox <kressemalte4@outlook.de>
2025-11-17 17:31:21 +0100

    mob effect

--- startup_scripts/main.js
+++ startup_scripts/main.js
@@ -10,3 +10,10 @@ StartupEvents.registry("item", event => {
     event.create("brian_item")
     .displayName("§7Brian Item")
 })
+
+StartupEvents.registry("mob_effect", event => {
+    event.create("crack").modifyAttribute("generic.movement_speed",
+        "d867190e-3726-4df0-a2f3-431b354c93a1",
+        5, "add_multiplied_base")
+        .color(Color.WHITE)
+})
